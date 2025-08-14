1b9d5d41a3206e3dded5a865971185364f3350d0
slc90 <506091625@qq.com>
2025-08-14 17:38:52 +0800

    使用自定义的字体

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use eframe::{
+    egui,
+    epaint::text::{FontInsert, InsertFontFamily},
+};
 use log::{debug, error, info, warn};
 
 fn main() -> eframe::Result {
@@ -12,12 +16,47 @@ fn main() -> eframe::Result {
     eframe::run_native(
         "learn rust",
         native_options,
-        Box::new(|_| Ok(Box::new(MyApp))),
+        Box::new(|cc| Ok(Box::new(MyApp::new(cc)))),
     )
 }
 
-struct MyApp;
+struct MyApp {
+    text: String,
+}
+
+impl MyApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // replace_fonts(&cc.egui_ctx);
+        add_font(&cc.egui_ctx);
+        Self {
+            text: "测试".to_string(),
+        }
+    }
+}
+
+// Demonstrates how to add a font to the existing ones
+fn add_font(ctx: &egui::Context) {
+    ctx.add_font(FontInsert::new(
+        "my_font",
+        egui::FontData::from_static(include_bytes!("../fonts/my_font.TTC")),
+        vec![
+            InsertFontFamily {
+                family: egui::FontFamily::Proportional,
+                priority: egui::epaint::text::FontPriority::Highest,
+            },
+            InsertFontFamily {
+                family: egui::FontFamily::Monospace,
+                priority: egui::epaint::text::FontPriority::Lowest,
+            },
+        ],
+    ));
+}
 
 impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {}
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("egui using custom fonts");
+            ui.text_edit_multiline(&mut self.text);
+        });
+    }
 }
