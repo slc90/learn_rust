d05b3b0bd78350cf578262bb79b181ab6c75272e
slc90 <506091625@qq.com>
2025-08-15 08:00:54 +0800

    启动callback特性,鼠标悬浮时,查看widget对应的源代码

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use eframe::{
-    egui,
-    epaint::text::{FontInsert, InsertFontFamily},
-};
+use eframe::egui;
+
+mod keypad;
+use keypad::Keypad;
 use log::{debug, error, info, warn};
 
 fn main() -> eframe::Result {
@@ -9,54 +9,69 @@ fn main() -> eframe::Result {
         std::env::set_var("RUST_LOG", "learn_rust=debug");
     };
     pretty_env_logger::init();
-    let native_options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([800.0, 600.0]),
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([640.0, 480.0]),
         ..Default::default()
     };
     eframe::run_native(
-        "learn rust",
-        native_options,
-        Box::new(|cc| Ok(Box::new(MyApp::new(cc)))),
+        "Custom Keypad App",
+        options,
+        Box::new(|cc| {
+            // Use the dark theme
+            cc.egui_ctx.set_theme(egui::Theme::Dark);
+            cc.egui_ctx.set_debug_on_hover(true);
+            // This gives us image support:
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+
+            Ok(Box::<MyApp>::default())
+        }),
     )
 }
 
 struct MyApp {
-    text: String,
+    name: String,
+    age: u32,
+    keypad: Keypad,
 }
 
-impl MyApp {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // replace_fonts(&cc.egui_ctx);
-        add_font(&cc.egui_ctx);
+impl MyApp {}
+
+impl Default for MyApp {
+    fn default() -> Self {
         Self {
-            text: "测试".to_string(),
+            name: "Arthur".to_owned(),
+            age: 42,
+            keypad: Keypad::new(),
         }
     }
 }
 
-// Demonstrates how to add a font to the existing ones
-fn add_font(ctx: &egui::Context) {
-    ctx.add_font(FontInsert::new(
-        "my_font",
-        egui::FontData::from_static(include_bytes!("../fonts/my_font.TTC")),
-        vec![
-            InsertFontFamily {
-                family: egui::FontFamily::Proportional,
-                priority: egui::epaint::text::FontPriority::Highest,
-            },
-            InsertFontFamily {
-                family: egui::FontFamily::Monospace,
-                priority: egui::epaint::text::FontPriority::Lowest,
-            },
-        ],
-    ));
-}
-
 impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("egui using custom fonts");
-            ui.text_edit_multiline(&mut self.text);
-        });
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::Window::new("Custom Keypad")
+            .default_pos([100.0, 100.0])
+            .title_bar(true)
+            .show(ctx, |ui| {
+                ui.horizontal(|ui| {
+                    ui.label("Your name: ");
+                    ui.text_edit_singleline(&mut self.name);
+                });
+                ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+                if ui
+                    .add_enabled(false, egui::Button::new("Can't click this"))
+                    .clicked()
+                {
+                    error!("button click");
+                }
+                if ui.button("Increment").clicked() {
+                    self.age += 1;
+                }
+                ui.label(format!("Hello '{}', age {}", self.name, self.age));
+            });
+        self.keypad.show(ctx);
+    }
+
+    fn raw_input_hook(&mut self, ctx: &egui::Context, raw_input: &mut egui::RawInput) {
+        self.keypad.bump_events(ctx, raw_input);
     }
 }
